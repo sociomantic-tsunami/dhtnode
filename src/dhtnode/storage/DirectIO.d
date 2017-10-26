75c74fd4ccdf17e1b6fa86c95560b522d39f5619
Gavin Norman <gavin.norman@sociomantic.com>
2017-10-26 12:21:39 +0200

    Use ocean facilities for safe DirectIO buffer allocation
    Fixes #4.

--- src/dhtnode/storage/DirectIO.d
+++ src/dhtnode/storage/DirectIO.d
@@ -247,7 +247,7 @@ public class BufferedDirectWriteTempFile : BufferedDirectWriteFile
     {
         this.suffix = suffix;
         this.disable_direct_io = disable_direct_io;
-        super(path, new ubyte[buffer_blocks * BLOCK_SIZE]);
+        super(path, buffer_blocks);
     }
 
     /***************************************************************************
@@ -386,7 +386,7 @@ public class BufferedDirectReadFile : ocean.io.device.DirectIO.BufferedDirectRea
             bool disable_direct_io = false )
     {
         this.disable_direct_io = disable_direct_io;
-        super(path, new ubyte[buffer_blocks * BLOCK_SIZE]);
+        super(path, buffer_blocks);
     }
 
     /***************************************************************************
