be7c7fcd3277b7c1dbd4160db7b3ac2c57647355
Nemanja Boric <nemanja.boric@sociomantic.com>
2018-03-05 12:26:17 +0100

    Add error message if checking for /srv/dhtnode/dhtnode-* fails
    dhtnode install script relies on /srv/dhtnode/dhtnode-* structure is setup.
    If it doesn't exist, instead of silent exit it should printout the error
    message.

--- pkg/after_dhtnode_install.sh
+++ pkg/after_dhtnode_install.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+# Exits with an error message
+error_exit()
+{
+    msg="$1"
+    code="$2"
+    echo "$msg" 1>&2
+    exit "$code"
+}
+
 if [ "$1" = "configure" ]; then
     addgroup --system core
     adduser --system --no-create-home dhtnode
 
     # Check that deployment directory exists
-    test -d /srv/dhtnode || exit 1
+    test -d /srv/dhtnode || error_exit "/srv/dhtnode/dhtnode-* directories missing" 1
 
     # Create directory to which dhtnode will write log files, if it does not
     # exist, and ensure proper permissions.
