6cbe92ece3cf750478793979097469d1b1cd7740
Mihails Strasuns <mihails.strasuns@gmail.com>
2018-09-04 16:19:54 +0300

    Switch to runtimebase image for Dockerfile
    Removes the need to add Sociomantic apt server manually

--- docker/install.sh
+++ docker/install.sh
@@ -4,13 +4,6 @@ set -xeu
 # Install dependencies
 
 apt update
-apt install -y lsb-release apt-transport-https
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-echo "deb https://dl.bintray.com/sociomantic-tsunami/dlang \
-    $(lsb_release -cs) release prerelease" >> /etc/apt/sources.list.d/dlang.list
-
-apt update
 apt install -y libebtree6
 
 # Prepare folder structure and install dhtnode
